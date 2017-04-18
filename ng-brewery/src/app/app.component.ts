34bf84364a8e1e012475c0215ae721feec2dcb06
Ryan Thissen <rmt1855@gmail.com>
2017-04-18 10:04:06 -0700

    super mvp

--- ng-brewery/src/app/app.component.ts
+++ ng-brewery/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
-}
+  title = 'Welcome to Angular Brewery!';
+  link = 'http://the-ipa-api.herokuapp.com/ipa-api/search?name=';
+  http: Http;
+  results = [];
+
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  performSearch(searchTerm: HTMLInputElement): void {
+    const apiLink = this.link + searchTerm.value;
+
+    this.http.request(apiLink)
+      .subscribe((res: Response) => {
+        this.results = res.json();
+        console.log('this is results: ', this.results);
+      });
+  }
+};
--- ng-brewery/src/main.ts
+++ ng-brewery/src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { HttpModule } from '@angular/http';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,4 +8,4 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule, [HttpModule]);
